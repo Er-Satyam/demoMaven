5433c0f705fa4c539189d7f65a4dbb814a715763
HP <HP@192.168.174.1>
2023-08-03 18:50:11 +0530

    project3

---
+++ demoMaven/src/test/java/com/demoMaven/testscript/CustomerModue.java
@@ -0,0 +1,19 @@
+package com.demoMaven.testscript;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModue {
+	@Test
+	public void customerModule() {
+		WebDriver driver=new ChromeDriver();
+		Reporter.log("createCustomer",true);
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+	}
+}
